1285b1982f24b78593b4b338ccb699b365a3f73a
Imran <ikhanmk@gmail.com>
2024-07-16 14:33:43 +0530

    add tpes, cart and login

--- Ecommerce-Frontend-2024/src/pages/cart.tsx
+++ Ecommerce-Frontend-2024/src/pages/cart.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { VscError } from "react-icons/vsc";
+import CartItemCard from "../components/cart-item";
+import { CartItem } from "../types/types";
+import {  useState } from "react";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
 
-export default Cart
+  const incrementHandler = (cartItem: CartItem) => {
+    if (cartItem.quantity >= cartItem.stock) return;
+  };
+  const decrementHandler = (cartItem: CartItem) => {
+    if (cartItem.quantity <= 1) return;
+  };
+  const removeHandler = (productId: string) => {
+  };
+  
+
+  return (
+    <div className="cart">
+      <main>
+        
+            (<CartItemCard
+              incrementHandler={incrementHandler}
+              decrementHandler={decrementHandler}
+              removeHandler={removeHandler}
+              key={'idx'}
+              cartItem={'i'}
+            />
+         ): (
+          <h1>No Items Added</h1>
+        )
+      </main>
+      <aside>
+        <p>Subtotal: ₹{9000}</p>
+        <p>Shipping Charges: ₹{150}</p>
+        <p>Tax: ₹{60}</p>
+        <p>
+          Discount: <em className="red"> - ₹{50}</em>
+        </p>
+        <p>
+          <b>Total: ₹{10000}</b>
+        </p>
+
+        <input
+          type="text"
+          placeholder="Coupon Code"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+        />
+
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              ₹{50} off using the <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupon <VscError />
+            </span>
+          ))}
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
