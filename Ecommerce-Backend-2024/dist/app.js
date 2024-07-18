ebc4b7f69953a218b324202805a96c872f7b6e05
Imran <ikhanmk@gmail.com>
2024-07-18 15:47:28 +0530

    initial setup and create user apis

---
+++ Ecommerce-Backend-2024/dist/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { errorMiddleware } from "./middlewares/error.js";
+import { connectDB } from "./utils/features.js";
+//import routes
+import userRoute from "./routes/userRoutes.js";
+connectDB("mongodb://localhost:27017/");
+const port = 4000;
+const app = express();
+app.use(express.json());
+// Using Routes
+app.use("/api/v1/user", userRoute);
+app.use(errorMiddleware);
+app.listen(port, () => {
+    console.log(`Server is working on http://localhost:${port}`);
+});
---
+++ Ecommerce-Backend-2024/src/routes/userRoutes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { deleteUser, getAllUsers, getUser, newUser } from "../controllers/userController.js";
+
+const app = express.Router();
+
+// route - /api/v1/user/new
+app.post("/new", newUser);
+
+app.get("/all",getAllUsers)
+
+app.route("/:id").get(getUser).delete(deleteUser);
+
+export default app;
